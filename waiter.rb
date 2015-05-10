019d447401da136c92a956fe28f57653bd071303
Dave Jachimiak <dave.jachimiak@gmail.com>
2015-05-10 14:38:18 -0400

    Release new waiter

--- waiter.rb
+++ waiter.rb
@@ -5,8 +5,8 @@ class Waiter < Formula
   include Language::Haskell::Cabal
 
   homepage "https://github.com/davejachimiak/waiter"
-  url "https://github.com/davejachimiak/waiter/archive/0.1.0.1.tar.gz"
-  sha256 "2c7ef7686c2c0c51be11035eb3dc06505e7f8cdb51e4d09aa370ac5840b02448"
+  url "https://github.com/davejachimiak/waiter/archive/0.1.0.2.tar.gz"
+  sha256 "3fa95c8a8dbfb8a81098aaa82902e1142223265fc31c5a997e36c14d5270c8d6"
 
   depends_on "cabal-install" => :build
   depends_on "ghc"
